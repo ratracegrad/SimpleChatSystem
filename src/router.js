50b7c4154b6f1798c0f5234f58cec5d21caf8eef
ChingChang9 <chingtheprogrammer@icloud.com>
2019-01-16 18:52:40 -0700

    Revert "history mode"
    This reverts commit 32291a90083b35cd5cea6b7a5d012d0ab4bebcb6.

--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ import Room from './views/Room.vue';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: '/',
@@ -21,7 +20,7 @@ export default new Router({
     {
       path: '/contact',
       name: 'contact',
-      component: () => import('./views/Contact.vue'),
+      component: () => import('./views/Contact.vue')
     },
     {
       path: '/guide',
